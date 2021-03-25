ce5e509fefde7e153b9d5df9eb4e5c8e675f280d
Joaquim Codina <joaquim.codina@e-campus.uab.cat>
2021-03-25 21:37:40 +0100

    Modificar funció get_score pel testeig

--- main/Game.py
+++ main/Game.py
@@ -1,10 +1,15 @@
 from main.Player import Player
 
+ZERO = ("Love", 0)
+FIFTEEN = ("Fifteen", 15)
+THIRTY = ("Thirty", 30)
+FORTY = ("Forty", 40)
 
 class Game:
     def __init__(self, player1: Player, player2: Player):
         self.__player1 = player1
         self.__player2 = player2
+        self.score = [ZERO, FIFTEEN, THIRTY, FORTY]
 
     def won_point(self, player: Player) -> None:
         if player == self.__player1:
@@ -19,14 +24,32 @@ class Game:
     def get_score(self) -> str:
         points_player1 = self.__player1.get_points()
         points_player2 = self.__player2.get_points()
+        res = "Love-All"
 
-        if points_player1 == 1 and points_player2 == 0:
-            return "Fifteen-Love"
+        if points_player1 <= 3 and points_player2 <= 3:
+            if points_player1 == points_player2:
+                if points_player1 >= 3 and points_player2 >= 3:
+                    return "Deuce"
+                else:
+                    res = self.score[points_player1][0] + "-All"
+            else:
+                res = self.score[points_player1][0] + "-" + self.score[points_player2][0]
+            return res
 
-        if points_player1 == 2 and points_player2 == 1:
-            return "Thirty-Fifteen"
+        if points_player1 == points_player2 and points_player1 >= 3 and points_player2 >= 3:
+            return "Deuce"
 
-        if points_player1 == 1 and points_player2 == 1:
-            return "Fifteen-All"
+        if self.__winner == self.__player1:
+            if abs(points_player1 - points_player2) >= 2 and points_player1 >= 4:
+                return "Win for player1"
+        else:
+            if abs(points_player2 - points_player1) >= 2 and points_player2 >= 4:
+                return "Win for player2"
 
-        return "Love-All"
+        if points_player1 >= 3 and points_player2 >= 3:
+            if points_player1 > points_player2:
+                return "Advantage player1"
+            elif points_player2 > points_player1:
+                return "Advantage player2"
+
+        return res
